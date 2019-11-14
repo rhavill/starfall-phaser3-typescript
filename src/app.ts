4e8ad303185f12aed55d09739df6aa413c458b46
Ricardo Havill <rhavill2002@yahoo.com>
2019-11-14 11:14:05 -0400

    Added basic game with a single scene.

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import "phaser";
+import { GameScene } from "./gameScene";
+
+const config: Phaser.Types.Core.GameConfig = {
+  title: "Starfall",
+  width: 800,
+  height: 600,
+  parent: "game",
+  scene: [GameScene],
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: false
+    }
+  },
+  backgroundColor: "#18216D"
+};
+
+export class StarfallGame extends Phaser.Game {
+  constructor(config: Phaser.Types.Core.GameConfig) {
+    super(config);
+  }
+}
+
+window.onload = () => {
+  var game = new StarfallGame(config);
+};
